ebf31f7b43cd8b7f80e6cbd7ac30ff113ed550e1
Xster-Cee <eonwuelu@gmail.com>
2023-08-09 21:53:20 +0100

    second commit

--- main.js
+++ main.js
@@ -85,37 +85,121 @@ const quizQuestions = [
 ]
 
 // declaration of variables
-const startButton = document.getElementById('start-btn');
-const finishButton = document.getElementById('finish-btn');
-const nextButton = document.getElementById('next-btn');
-const instructP = document.getElementsByClassName('instruct');
-const questionSection = document.getElementById('question-holder');
-const questionLine = document.getElementById('question');
-const scoring = document.getElementById('answer');
-const quiz = document.getElementById('quiz');
+// const startButton = document.getElementById('start-btn');
+// const finishButton = document.getElementById('finish-btn');
+// const nextButton = document.getElementById('next-btn');
+// const instruct = document.querySelector('.instruct');
+// const questionSection = document.getElementById('question-holder');
+// const questionLine = document.getElementById('question');
+// const scoring = document.getElementById('score');
+// const quiz = document.getElementById('quiz');
+// const answerColumn = document.getElementById('answer');
+// let shuffledQuestion, currentQuestionIndex;
+// let score = 0;
+
+// eventlisteners for when an event occurs
+// startButton.addEventListener("click", startGame);
+// finishButton.addEventListener("click", endGame);
+// nextButton.addEventListener("click", ()=>{
+//     currentQuestionIndex++;
+//     setNextQuestion();
+// });
+
+// function startGame() {
+//     quiz.classList.add('hide');
+//     startButton.classList.add('hide');
+//     finishButton.classList.add('hide');
+//     questionSection.classList.remove('hide');
+//     shuffledQuestion = questions.sort(()=>Math.random()-0.6);
+//     currentQuestionIndex = 0;
+//     scoring.innerText = "Score: " + score;
+//     setNextQuestion();
+// }
+
+// function setNextQuestion() {
+//     resetState();
+//     showQuestion(shuffledQuestion[currentQuestionIndex]);
+// }
+
+// function showQuestion(question) {
+//     questionLine.innerText = question.question;
+//     question.answers.forEach((answer) => {
+//         const button = document.createElement('button');
+//         button.innerText = answer.text;
+//         button.classList.add('btn')
+//         if(answer.correct) {
+//             button.dataset.correct = answer.correct;
+//         }button.addEventListener("click", selectAnswer);
+//         answerColumn.appendChild(button);
+//     });
+// }
+
+// function resetState() {
+//     nextButton.classList.add('hide');
+//     while(answerColumn.firstChild) {
+//         answerColumn.removeChild(answerColumn.firstChild);
+//     }
+// }
+
+//declaration of variables
+const startButton = document.getElementById("start-btn");
+const nextButton = document.getElementById("next-btn");
+const instruct = document.querySelector(".instruct");
+const finishButton = document.getElementById("finish-btn");
+const quiz = document.getElementById("quiz");
+const questionSection = document.getElementById("question-holder");
+const questionLine = document.getElementById("question");
+const answerColumn = document.getElementById("answer");
+const scoring = document.getElementById("score");
+
 let shuffledQuestion, currentQuestionIndex;
 let score = 0;
 
 // eventlisteners for when an event occurs
 startButton.addEventListener("click", startGame);
 finishButton.addEventListener("click", endGame);
-nextButton.addEventListener("click", ()=>{
-    currentQuestionIndex++;
-    setNextQuestion();
+nextButton.addEventListener("click", () => {
+  currentQuestionIndex++;
+  setNextQuestion();
 });
 
+// when start button is clicked
 function startGame() {
-    quiz.classList.add('hide');
-    startButton.classList.add('hide');
-    finishButton.classList.add('hide');
-    questionSection.classList.remove('hide');
-    shuffledQuestion = questions.sort(()=>Math.random()-0.6);
-    currentQuestionIndex = 0;
-    scoring.innerText = "Score: " + score;
-    setNextQuestion();
+  quiz.classList.add("hide");
+  startButton.classList.add("hide");
+  finishButton.classList.add("hide");
+  questionSection.classList.remove("hide");
+  shuffledQuestion = questions.sort(() => Math.random() - 0.6);
+  currentQuestionIndex = 0;
+  scoring.innerText = "Score: " + score;
+  setNextQuestion();
 }
 
+// selecting next question
 function setNextQuestion() {
-    resetState();
-    showQuestion(shuffledQuestion[currentQuestionIndex]);
+  resetState();
+  showQuestion(shuffledQuestion[currentQuestionIndex]);
+}
+
+
+function showQuestion(question) {
+  questionLine.innerText = question.question;
+  question.answers.forEach((answer) => {
+    const button = document.createElement("button");
+    button.innerText = answer.text;
+    button.classList.add("btn");
+    if (answer.correct) {
+      button.dataset.correct = answer.correct;
+    }
+    button.addEventListener("click", selectAnswer);
+    answerColumn.appendChild(button);
+  });
+}
+
+// removing next button and previous children button of the answer div
+function resetState() {
+  nextButton.classList.add("hide");
+  while (answerColumn.firstChild) {
+    answerColumn.removeChild(answerColumn.firstChild);
+  }
 }
